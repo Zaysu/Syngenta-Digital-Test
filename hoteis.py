eb93d07d0b6932abb2c49ee3b6177db31610b3bd
Zaysu <80471248+Zaysu@users.noreply.github.com>
2022-05-06 11:53:31 -0300

    2.1
    Detalhes

--- hoteis.py
+++ hoteis.py
@@ -47,81 +47,84 @@ class Hoteis:
         
         data = self.dataSaida - self.dataEntrada
         
-        #Conferindo se alguma entrada cai no fim de semana
-        if self.tipoCliente == "Regular" and self.dataEntrada in fimdesemana:
-            if data * lakewoodNormal < data * bridgewoodNormal1 and data * lakewoodNormal < data * ridgewoodNormal2:
-                return hoteis[0]
+        if self.dataEntrada >= 1 and self.dataEntrada < 32 and self.dataSaida >= 1 and self.dataSaida < 32 and self.dataEntrada < self.dataSaida:
             
-            elif data * bridgewoodNormal1 < data * lakewoodNormal and data * bridgewoodNormal1 < data * ridgewoodNormal2:
-                return hoteis[1]
+            #Conferindo se alguma entrada cai no fim de semana
+            if self.tipoCliente == "Regular" and self.dataEntrada in fimdesemana:
+                if data * lakewoodNormal < data * bridgewoodNormal1 and data * lakewoodNormal < data * ridgewoodNormal2:
+                    return hoteis[0]
+                
+                elif data * bridgewoodNormal1 < data * lakewoodNormal and data * bridgewoodNormal1 < data * ridgewoodNormal2:
+                    return hoteis[1]
+                
+                elif data * ridgewoodNormal2 < data * lakewoodNormal and data * ridgewoodNormal2 < data * bridgewoodNormal1:
+                    return hoteis[2]
+                
+            elif self.tipoCliente == "Reward" and self.dataEntrada in fimdesemana:
+                if data * lakewoodReward < data * bridgewoodReward2 and data * lakewoodReward < data * ridgewoodReward2:
+                    return hoteis[0]
+                
+                elif data * bridgewoodReward2 < data * lakewoodReward and data * bridgewoodReward2 < data * ridgewoodReward2:
+                    return hoteis[1]
+                
+                elif data * ridgewoodReward2 < data * lakewoodReward and data * ridgewoodReward2 < data * bridgewoodReward2:
+                    return hoteis[2]
             
-            elif data * ridgewoodNormal2 < data * lakewoodNormal and data * ridgewoodNormal2 < data * bridgewoodNormal1:
-                return hoteis[2]
+            #Conferindo se alguma saida cai no fim de semana
+            elif self.tipoCliente == "Regular" and self.dataSaida in fimdesemana:
+                if data * lakewoodNormal < data * bridgewoodNormal1 and data * lakewoodNormal < data * ridgewoodNormal2:
+                    return hoteis[0]
+                
+                elif data * bridgewoodNormal1 < data * lakewoodNormal and data * bridgewoodNormal1 < data * ridgewoodNormal2:
+                    return hoteis[1]
+                
+                elif data * ridgewoodNormal2 < data * lakewoodNormal and data * ridgewoodNormal2 < data * bridgewoodNormal1:
+                    return hoteis[2]
+                
+            elif self.tipoCliente == "Reward" and self.dataSaida in fimdesemana:
+                if data * lakewoodReward < data * bridgewoodReward2 and data * lakewoodReward < data * ridgewoodReward2:
+                    return hoteis[0]
+                
+                elif data * bridgewoodReward2 < data * lakewoodReward and data * bridgewoodReward2 < data * ridgewoodReward2:
+                    return hoteis[1]
+                
+                elif data * ridgewoodReward2 < data * lakewoodReward and data * ridgewoodReward2 < data * bridgewoodReward2:
+                    return hoteis[2]
             
-        elif self.tipoCliente == "Reward" and self.dataEntrada in fimdesemana:
-            if data * lakewoodReward < data * bridgewoodReward2 and data * lakewoodReward < data * ridgewoodReward2:
-                return hoteis[0]
+                
+            elif self.tipoCliente == "Regular":
+                if data * normallakewood < data * normabridgewood and data * normallakewood < data * normalridgewood:
+                    return hoteis[0]
+                
+                elif data * normabridgewood < data * normallakewood and data * normabridgewood < data * normalridgewood:
+                    return hoteis[1]
+                
+                elif data * normalridgewood < data * normallakewood and data * normalridgewood < data * normabridgewood:
+                    return hoteis[2]
             
-            elif data * bridgewoodReward2 < data * lakewoodReward and data * bridgewoodReward2 < data * ridgewoodReward2:
-                return hoteis[1]
+            elif self.tipoCliente == "Reward":
+                if data * rewardlakewood < data * rewardbridgewood and data * rewardlakewood < data * rewardridgewood:
+                    return hoteis[0]
+                
+                elif data * rewardbridgewood < data * rewardlakewood and data * rewardbridgewood < data * rewardridgewood:
+                    return hoteis[1]
+                
+                elif data * rewardridgewood < data * rewardbridgewood and data * rewardridgewood < data * rewardlakewood:
+                    return hoteis[2]
             
-            elif data * ridgewoodReward2 < data * lakewoodReward and data * ridgewoodReward2 < data * bridgewoodReward2:
-                return hoteis[2]
-        
-        #Conferindo se alguma saida cai no fim de semana
-        elif self.tipoCliente == "Regular" and self.dataSaida in fimdesemana:
-            if data * lakewoodNormal < data * bridgewoodNormal1 and data * lakewoodNormal < data * ridgewoodNormal2:
-                return hoteis[0]
-            
-            elif data * bridgewoodNormal1 < data * lakewoodNormal and data * bridgewoodNormal1 < data * ridgewoodNormal2:
-                return hoteis[1]
-            
-            elif data * ridgewoodNormal2 < data * lakewoodNormal and data * ridgewoodNormal2 < data * bridgewoodNormal1:
-                return hoteis[2]
-            
-        elif self.tipoCliente == "Reward" and self.dataSaida in fimdesemana:
-            if data * lakewoodReward < data * bridgewoodReward2 and data * lakewoodReward < data * ridgewoodReward2:
-                return hoteis[0]
-            
-            elif data * bridgewoodReward2 < data * lakewoodReward and data * bridgewoodReward2 < data * ridgewoodReward2:
-                return hoteis[1]
-            
-            elif data * ridgewoodReward2 < data * lakewoodReward and data * ridgewoodReward2 < data * bridgewoodReward2:
-                return hoteis[2]
-        
-            
-        elif self.tipoCliente == "Regular":
-            if data * normallakewood < data * normabridgewood and data * normallakewood < data * normalridgewood:
-                return hoteis[0]
-            
-            elif data * normabridgewood < data * normallakewood and data * normabridgewood < data * normalridgewood:
-                return hoteis[1]
-            
-            elif data * normalridgewood < data * normallakewood and data * normalridgewood < data * normabridgewood:
-                return hoteis[2]
-         
-        elif self.tipoCliente == "Reward":
-            if data * rewardlakewood < data * rewardbridgewood and data * rewardlakewood < data * rewardridgewood:
-                return hoteis[0]
-            
-            elif data * rewardbridgewood < data * rewardlakewood and data * rewardbridgewood < data * rewardridgewood:
-                return hoteis[1]
-            
-            elif data * rewardridgewood < data * rewardbridgewood and data * rewardridgewood < data * rewardlakewood:
-                return hoteis[2]
-        
-        ######-----------desempate-----------########
-            
-        elif data * normallakewood == data * bridgewoodNormal1 and data * normallakewood == data * ridgewoodNormal2:
-            if hoteis[0].values() < hoteis[1].values() and hoteis[0].values() < hoteis[2].values():
-                return hoteis[0]
-            
-        elif data * bridgewoodNormal1 == data * lakewoodNormal and data * bridgewoodNormal1 == data * ridgewoodNormal2:
-            if hoteis[1].values() < hoteis[0].values() and hoteis[1].values() < hoteis[2].values():
-                return hoteis[1]
-            
-        elif data * ridgewoodNormal2 == data * lakewoodNormal and data * ridgewoodNormal2 == data * bridgewoodNormal1:
-            if hoteis[2].values() < hoteis[0].values() and hoteis[2].values() < hoteis[1].values():
-                return hoteis[2]
-            
-            
+            ######-----------desempate-----------########
+                
+            elif data * normallakewood == data * bridgewoodNormal1 and data * normallakewood == data * ridgewoodNormal2:
+                if hoteis[0].values() < hoteis[1].values() and hoteis[0].values() < hoteis[2].values():
+                    return hoteis[0]
+                
+            elif data * bridgewoodNormal1 == data * lakewoodNormal and data * bridgewoodNormal1 == data * ridgewoodNormal2:
+                if hoteis[1].values() < hoteis[0].values() and hoteis[1].values() < hoteis[2].values():
+                    return hoteis[1]
+                
+            elif data * ridgewoodNormal2 == data * lakewoodNormal and data * ridgewoodNormal2 == data * bridgewoodNormal1:
+                if hoteis[2].values() < hoteis[0].values() and hoteis[2].values() < hoteis[1].values():
+                    return hoteis[2]
+                
+        else:
+             return "Não há disponibilidade de hotel para o período solicitado"
